8c141aef87289e483357ffab0e14b877c0e2cc43
blabla <blablablabla@gmail.com>
2022-03-25 12:00:48 -0300

    blabla

---
+++ menus/dono.js
@@ -0,0 +1,73 @@
+const menudono = (prefix) => {
+	
+	
+// NÃO APAGUE ESSE ${prefix}, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa. 	
+
+return `
+*╔═══❖•ೋ°ৡৢ͜͡  ❤‍🔥 𖥨֗°ೋ•❖═══╗*
+                     ᬊ᭄ 𝐌 𝐄 𝐒 𝐓 𝐑 𝐄 ᬊ᭄
+*╚═══❖•ೋ°ৡৢ͜͡  ❤‍🔥 𖥨֗°ೋ•❖═══╝*
+_𝙴𝚜𝚝𝚎 𝚎́ 𝚞𝚖 𝙼𝚎𝚗𝚞 𝙴𝚡𝚌𝚕𝚞𝚜𝚒𝚟𝚘 𝙿𝚊𝚛𝚊 𝙼𝚎𝚞 𝙳𝚘𝚗𝚘._
+
+║ঔৣ͜͡҉ 💧 ${prefix}Fotomenu (marcar)
+║ঔৣ͜͡҉ 💧 ${prefix}Bockcmd  (cmd)
+║ঔৣ͜͡҉ 💧 ${prefix}Unblockcmd (cmd)
+║ঔৣ͜͡҉ 💧 ${prefix}Legendabv (oq qr)
+║ঔৣ͜͡҉ 💧 ${prefix}Legendasaiu (oq qr)
+║ঔৣ͜͡҉ 💧 ${prefix}Legendasaiu2 (oq qr)
+║ঔৣ͜͡҉ 💧 ${prefix}Legendabv2 (oq qr)
+║ঔৣ͜͡҉ 💧 ${prefix}Fundobemvindo (marcar-img)
+║ঔৣ͜͡҉ 💧 ${prefix}Gundosaiu (marcar-img)
+║ঔৣ͜͡҉ 💧 ${prefix}Serpremium
+║ঔৣ͜͡҉ 💧 ${prefix}Listabt
+║ঔৣ͜͡҉ 💧 ${prefix}Listagp
+║ঔৣ͜͡҉ 💧 ${prefix}Limparmsg (chat-do-bot)
+║ঔৣ͜͡҉ 💧 ${prefix}Antipalavrão 1 / 0
+║ঔৣ͜͡҉ 💧 ${prefix}Antiligar 1 / 0
+║ঔৣ͜͡҉ 💧 ${prefix}Antipv 1 / 0 (esse da block)
+║ঔৣ͜͡҉ 💧 ${prefix}Addpalavra (palavrão)
+║ঔৣ͜͡҉ 💧 ${prefix}Delpalavra (palavrão)
+║ঔৣ͜͡҉ 💧 ${prefix}Antipvon
+║ঔৣ͜͡҉ 💧 ${prefix}Antipvoff
+║ঔৣ͜͡҉ 💧 ${prefix}Ativo
+║ঔৣ͜͡҉ 💧 ${prefix}Ausente (fale-oq-faz)
+║ঔৣ͜͡҉ 💧 ${prefix}Delpremium @(marca)
+║ঔৣ͜͡҉ 💧 ${prefix}Addpremium @(marca)
+║ঔৣ͜͡҉ 💧 ${prefix}Clonar [@] (rouba ft de prf)
+║ঔৣ͜͡҉ 💧 ${prefix}Fotobot (img, = foto do BT)
+║ঔৣ͜͡҉ 💧 ${prefix}Descriçãogp (digite-algo)
+║ঔৣ͜͡҉ 💧 ${prefix}Limpar (limpa tds conversas)
+║ঔৣ͜͡҉ 💧 ${prefix}Block [@] (block cmds) 
+║ঔৣ͜͡҉ 💧 ${prefix}Unblock [@] (desbloquear) 
+║ঔৣ͜͡҉ 💧 ${prefix}Setprefix  (prefixo-novo)
+║ঔৣ͜͡҉ 💧 ${prefix}Bangp
+║ঔৣ͜͡҉ 💧 ${prefix}Unbangp
+║ঔৣ͜͡҉ 💧 ${prefix}Dono2 @marca
+║ঔৣ͜͡҉ 💧 ${prefix}Dono3 @marca
+║ঔৣ͜͡҉ 💧 ${prefix}Dono4 @marca
+║ঔৣ͜͡҉ 💧 ${prefix}Dono5 @marca
+║ঔৣ͜͡҉ 💧 ${prefix}Bcgp (TM pra tds do gp)
+║ঔৣ͜͡҉ 💧 ${prefix}Transmitir -all / -pvs / -gps
+║ঔৣ͜͡҉ 💧 ${prefix}Transmitir2 (marcar)
+║ঔৣ͜͡҉ 💧 ${prefix}Setnomedobot  (novonome/bot)
+║ঔৣ͜͡҉ 💧 ${prefix}Setnickdono  (renomeanick/dono)
+║ঔৣ͜͡҉ 💧 ${prefix}Setnumerodono(númerodono/novo)obs:isso caso seja necessário
+║ঔৣ͜͡҉ 💧 ${prefix}Javolto
+║ঔৣ͜͡҉ 💧 ${prefix}Javoltoadm
+║ঔৣ͜͡҉ 💧 ${prefix}Seradm (caso seu bot seja adm)
+║ঔৣ͜͡҉ 💧 ${prefix}Semoverdono (caso seu bot seja adm)
+║ঔৣ͜͡҉ 💧 ${prefix}Rebaixardn (caso seu bot seja adm)
+║ঔৣ͜͡҉ 💧 ${prefix}Joinlink (linkgp)
+║ঔৣ͜͡҉ 💧 ${prefix}Dono/Bot (obs:com isso seu bot irá te remover e dps ela irá sair do grupo)
+║
+『${NomeDoBot}』`
+}
+
+exports.menudono = menudono
+
+
+
+
+
+
+
---
+++ menus/infodono.js
@@ -0,0 +1,35 @@
+const infodono = (prefix, numerodn) => {
+
+// NÃO APAGUE ESSE ${NickDono} nem 
+//${numerodn} nem ${NomeDoBot} nem ${prefix} só se quiser apagar completo, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa.
+
+return`
+*╔═══❖•ೋ°ৡৢ͜͡  ❤‍🔥 𖥨֗°ೋ•❖═══╗*
+                    ᬊ᭄𝐌𝐄𝐔 𝐃𝐎𝐍𝐎ᬊ᭄
+*╚═══❖•ೋ°ৡৢ͜͡  ❤‍🔥 𖥨֗°ೋ•❖═══╝*
+_𝙰𝚚𝚞𝚒  𝙴𝚜𝚝𝚊́ 𝙰𝚕𝚐𝚞𝚖𝚊𝚜 𝙸𝚗𝚏𝚘𝚛𝚖𝚊𝚌̧𝚘̃𝚎𝚜 𝚂𝚘𝚋𝚛𝚎 𝙼𝚎𝚞 𝙳𝚘𝚗𝚘._
+
+
+*║ঔৣ͜͡ீ͜❥͜  𝐍𝐎𝐌𝐄:*  ${NickDono} 
+*║ঔৣ͜͡ீ͜❥͜  𝐂𝐇𝐀𝐓:* wa.me/${numerodnofc} 
+*║ঔৣ͜͡ீ͜❥͜  𝐄𝐒𝐓𝐀𝐃𝐎:* Minas Gerais
+
+*║ঔৣ͜͡ீ͜❥͜ 𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌:* @Licht.Offc
+*║ঔৣ͜͡ீ͜❥͜  𝐋𝐈𝐍𝐊:* https://instagram.com/licht.offc?utm_medium=copy_link
+*║ঔৣ͜͡ீ͜❥͜  𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌:* @AquaBot.Wpp
+*║ঔৣ͜͡ீ͜❥͜  𝐋𝐈𝐍𝐊:* https://instagram.com/aquabot.wpp?utm_medium=copy_link
+*║ঔৣ͜͡ீ͜❥͜  𝐘𝐎𝐔𝐓𝐔𝐁𝐄:* LICHT SAN
+*║ঔৣ͜͡ீ͜❥͜  𝐋𝐈𝐍𝐊:* https://youtube.com/channel/UCCy_5Qpvgil8r-LyP9JF9Rw
+*║ঔৣ͜͡ீ͜❥͜ 𝐆𝐑𝐔𝐏𝐎 𝐎𝐅𝐈𝐂𝐈𝐀𝐋:*  ঔৣ͜͡ீ͜❥͜𝐀𝐐𝐔𝐀 𝐁𝐎𝐓ू ፝͜❥
+*║ঔৣ͜͡ீ͜❥͜  𝐋𝐈𝐍𝐊 :* https://chat.whatsapp.com/GbzIhcBEuR3FTfOO0g5vYz
+
+
+
+💦  𝐏𝐚𝐫𝐚 𝐀𝐝𝐢𝐜𝐢𝐨𝐧𝐚𝐫 𝐀𝐪𝐮𝐚𝐁𝐨𝐭 𝐍𝐨 𝐒𝐞𝐮 𝐆𝐫𝐮𝐩𝐨, 𝐔𝐥𝐭𝐢𝐥𝐢𝐳𝐞:
+𝐂𝐨𝐦𝐚𝐧𝐝𝐨: ${prefix}planos
+
+║ᬊ᭄${NomeDoBot} ᬊ᭄
+`
+}
+
+exports.infodono = infodono
